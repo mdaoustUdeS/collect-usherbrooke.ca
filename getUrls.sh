07c6d0c7805d5872a211960d3df6425a7ed498a1
mdaoustUdeS <4569756-mdaoustUdeS@users.noreply.gitlab.com>
2021-06-15 21:57:26 -0400

    Add data source (hebergement)

--- getUrls.sh
+++ getUrls.sh
@@ -21,3 +21,7 @@ wget -rD $host -k -l1 --accept $acceptedFiles --include $includedDirectories --d
 url="https://www.usherbrooke.ca/genie/faculte/coronavirus/"
 includedDirectories="genie/faculte/coronavirus"
 wget -rD $host -k -l1 --accept $acceptedFiles --include $includedDirectories --directory-prefix=$outputDirectory -- $url
+
+url="https://www.usherbrooke.ca/hebergement/covid/"
+includedDirectories="hebergement/covid"
+wget -rD $host -k -l1 --accept $acceptedFiles --include $includedDirectories --directory-prefix=$outputDirectory -- $url
