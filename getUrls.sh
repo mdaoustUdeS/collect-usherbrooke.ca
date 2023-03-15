0cbde70915cfd3b2273e7ad12feb1bd885c45463
mdaoustUdeS <4569756-mdaoustUdeS@users.noreply.gitlab.com>
2023-03-14 20:19:05 -0400

    Re-Rename page covid-19 to covid

--- getUrls.sh
+++ getUrls.sh
@@ -6,10 +6,6 @@ url="https://www.usherbrooke.ca/coronavirus/"
 includedDirectories="coronavirus"
 wget -rD $host -k -l1 --accept $acceptedFiles --include $includedDirectories --directory-prefix=$outputDirectory -- $url
 
-#url="https://www.usherbrooke.ca/hebergement/covid/"
-#includedDirectories="hebergement/fileadmin"
-#wget -rD $host -k -l1 --accept $acceptedFiles --include $includedDirectories --directory-prefix=$outputDirectory -- $url
-
-url="https://www.usherbrooke.ca/hebergement/covid-19/"
+url="https://www.usherbrooke.ca/hebergement/covid/"
 includedDirectories="hebergement/fileadmin"
 wget -rD $host -k -l1 --accept $acceptedFiles --include $includedDirectories --directory-prefix=$outputDirectory -- $url
