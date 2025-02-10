1bec75475693f94316774ef6cf02e60251b6a861
mdaoustUdeS <54960692+mdaoustUdeS@users.noreply.github.com>
2025-02-09 23:07:43 -0500

    Add old version of Thunderbird as useragent

--- getUrls.sh
+++ getUrls.sh
@@ -4,4 +4,4 @@ outputDirectory=html
 
 url="https://www.usherbrooke.ca/coronavirus/"
 includedDirectories="coronavirus"
-wget -rD $host -k -l1 --accept $acceptedFiles --include $includedDirectories --directory-prefix=$outputDirectory -- $url
+wget -rD $host -k -l1 --accept $acceptedFiles --include $includedDirectories --directory-prefix=$outputDirectory -U "Mozilla/5.0 (Windows; U; Windows NT 5.1; fr-FR; rv:1.7.5) Gecko/20041206 Thunderbird/1.0" -- $url
